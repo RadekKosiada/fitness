5faa3cdccc60ac0f9617dfccc2fe11516b8b6c64
RadekKosiada <kosiadar@gmail.com>
2021-05-09 11:55:48 +0200

    Filter entries according to category; Filter date WIP

--- frontend/src/components/Data.js
+++ frontend/src/components/Data.js
@@ -10,8 +10,9 @@ const optionsForDate = {
 
 export default function Data(props) {
   const [workouts, setWorkouts] = useState([]);
+  // const [sortedWorkouts, setSorterWorkouts] = useState([]);
 
-  console.log('Data props: ', props);
+  console.log("Data props: ", props);
 
   useEffect(() => {
     fetch("/data")
@@ -21,7 +22,6 @@ export default function Data(props) {
         }
       })
       .then(jsonResponse => {
-        console.log(jsonResponse);
         setWorkouts(jsonResponse);
       })
       .catch(error => {
@@ -29,31 +29,76 @@ export default function Data(props) {
       });
   }, []);
 
+  // useEffect(() => {
+  //   console.log('selectedDateOrder: ', props.selectedDateOrder);
+  //   if(props.selectedDateOrder === "descending") {
+  //     let newWorkouts = workouts.sort(function(a, b) {
+  //       // console.log(a, b)
+  //       return new Date(b.startDate) - new Date(a.startDate);
+  //   });
+  //   setSorterWorkouts(newWorkouts);
+  //   } else if (props.selectedDateOrder === "ascending") {
+  //     let newWorkouts = workouts.sort(function(b, a) {
+  //       // console.log(a, b)
+  //       return new Date(b.startDate) - new Date(a.startDate);
+  //   });
+  //   setSorterWorkouts(newWorkouts);
+  //   }
+
+  // }, [props.selectedDateOrder]);
+
+  // let newWorkouts = workouts.length ? workouts : [];
+  // console.log("NW2", workouts);
+  // if (props.selectedDateOrder === "descending") {
+  //   newWorkouts.sort(function(a, b) {
+  //     // console.log(a, b)
+  //     return new Date(b.startDate) - new Date(a.startDate);
+  //   });
+  // } else if (props.selectedDateOrder === "ascending") {
+  //   newWorkouts.sort(function(b, a) {
+  //     // console.log(a, b)
+  //     return new Date(b.startDate) - new Date(a.startDate);
+  //   });
+  // }
+  // else if (!props.selectedDateOrder.length) {
+  //   newWorkouts = workouts;
+  //   console.log('nW: ', workouts);
+  // }
+
+  let newWorkouts = [];
+  const categoriesOptions = ["", "c1", "c2", "c3", "c5", "c6", "c7"];
+  for(let i = 1; i <= categoriesOptions.length; i++) {
+    if (props.selectedCategory === categoriesOptions[i]) {
+      console.log(categoriesOptions[i]);
+      newWorkouts = workouts.filter((workout) => {
+        return workout.category === categoriesOptions[i];
+      })
+      console.log(newWorkouts);
+    } else if (props.selectedCategory === categoriesOptions[0]) {
+      newWorkouts = workouts;
+    }
+  }
+  
   return (
     <div>
-      {workouts
-        .sort(function(a, b) {
-          // console.log(a, b)
-          return new Date(b.startDate) - new Date(a.startDate);
-      })
-        .map(function(workout, i) {
-          return (
-            <div key={workout.index}>
-              <h1>
-                Name: {workout.name}, Index: {workout.index}
-              </h1>
-              <p>Description: {workout.description}</p>
-              <p>
-                Start date:{" "}
-                {new Date(workout.startDate).toLocaleDateString(
-                  lang,
-                  optionsForDate
-                )}
-              </p>
-              <p>Category: {workout.category}</p>
-            </div>
-          );
-        })}
+      {newWorkouts.map(function(workout, i) {
+        return (
+          <div key={workout.index}>
+            <h1>
+              Name: {workout.name}, Index: {workout.index}
+            </h1>
+            <p>Description: {workout.description}</p>
+            <p>
+              Start date:{" "}
+              {new Date(workout.startDate).toLocaleDateString(
+                lang,
+                optionsForDate
+              )}
+            </p>
+            <p>Category: {workout.category}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }

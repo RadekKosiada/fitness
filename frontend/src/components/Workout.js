83007db3dba93170a07d38bb2f846c5d928c7fee
RadekKosiada <kosiadar@gmail.com>
2021-05-09 20:01:36 +0200

    Create Workout component

---
+++ frontend/src/components/Workout.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Workout(props) {
+
+  const lang = document.documentElement.lang;
+  const optionsForDate = {
+    weekday: "short",
+    year: "numeric",
+    month: "short",
+    day: "numeric"
+  };
+
+  return (
+    <div className="workout-container" key={props.index}>
+      <h1>
+        Name: {props.name}, Index: {props.index}
+      </h1>
+      <p>Description: {props.description}</p>
+      <p>
+        Start date:{" "}
+        {new Date(props.startDate).toLocaleDateString(lang, optionsForDate)}
+      </p>
+      <p>Category: {props.category}</p>
+    </div>
+  );
+}

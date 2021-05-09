7cad213f891bf3cdbe7dbc75d6462d5abeed888b
RadekKosiada <kosiadar@gmail.com>
2021-05-09 13:57:10 +0200

    Add param in get in server.js for pagination

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-const data = require('./workouts40.min.json');
+const data = require('./workouts40.json');
 
-app.get("/data", (request, response) => {
+app.get("/data/:index", (request, response) => {
+  console.log(request.params.index)
   const sampleDate = data;
   response.json(sampleDate);
 });

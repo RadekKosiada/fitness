64537744aa9ec9f78bcf731f2a76878834c61228
RadekKosiada <kosiadar@gmail.com>
2021-05-09 23:59:18 +0200

    Delete code for resetting filters (not needed)

--- frontend/src/components/TopBar.js
+++ frontend/src/components/TopBar.js
@@ -1,25 +1,18 @@
-import React, { useState, useCallback } from "react";
+import React from "react";
 import Filter from "./Filter";
 
 export default function TopBar(props) {
-  // const handleClick = () => {
-  //   console.log("triggered");
-  //   setSelectedDateOrder(dateOptions[0]);
-  //   setselectedCategory(categoriesOptions[0]);
-  // };
 
   return (
     <div className="topbar-container">
       <h3>Filter entries:</h3>
       <Filter
-        // selectedFilter={selectedDateOrder}
         options={props.dateOptions}
         callback={props.callbackDate}
         selectedOption={props.selectedDateOrder}
         label={props.filterDateLabel}
       />
       <Filter
-        // selectedFilter={selectedCategory}
         options={props.categoriesOptions}
         callback={props.callbackCategories}
         selectedOption={props.selectedCategory}
@@ -27,7 +20,6 @@ export default function TopBar(props) {
         disabled={true}
       />
 
-      {/* <button onClick={handleClick}>Reset all filter</button> */}
     </div>
   );
 }

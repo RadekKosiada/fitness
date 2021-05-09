72203115faff14dfbe5654a077db875a909706de
RadekKosiada <kosiadar@gmail.com>
2021-05-09 14:32:11 +0200

    Set indexes to slice data to be sent to frontend

--- server.js
+++ server.js
@@ -2,12 +2,36 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-const data = require('./workouts40.json');
+const workouts = require("./workouts40.json");
 
 app.get("/data/:index", (request, response) => {
-  console.log(request.params.index)
-  const sampleDate = data;
-  response.json(sampleDate);
+  console.log(request.params.index);
+
+  const index = Number(request.params.index);
+  const entriesPerPage = 20;
+  const sumOfAllFrontendPages = workouts.length / entriesPerPage;
+
+  var indexOfFirstWorkoutToSend;
+  if (index === 1) {
+    indexOfFirstWorkoutToSend = 0;
+  } else {
+    indexOfFirstWorkoutToSend = index - 1 + index * 20;
+  }
+
+  var indexOfLastWorkoutToSend;
+  if (Number(index) === 1) {
+    indexOfLastWorkoutToSend = 20;
+  } else {
+    indexOfLastWorkoutToSend = index - 2 + index * 20;
+  }
+
+  console.log("Index last", indexOfLastWorkoutToSend);
+  console.log("Index first", indexOfFirstWorkoutToSend);
+
+  response.json({
+    workouts: workouts,
+    sumOfPages: sumOfAllFrontendPages
+  });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));

395187f18bcce70bee33306dced16d416b8937db
RadekKosiada <kosiadar@gmail.com>
2021-05-16 16:21:11 +0200

    Delete unused code & adjust formatting in Pagination

--- frontend/src/components/Pagination.js
+++ frontend/src/components/Pagination.js
@@ -8,22 +8,14 @@ export default function Pagination(props) {
   console.log(props);
 
   const currentPageNumber = Number(props.currentPage);
-  // if (currentPageNumber === 1) {
-    for (let i = 1; i <= props.sumOfPages; i++) {
-      pagesArray.push(i);
-    }
-  // } else if (1 < currentPageNumber < 50) {
-  //   for (let i = 1; i <= props.sumOfPages; i++) {
-  //     pagesArray.push(i);
-  //   }
-  // }
+  for (let i = 1; i <= props.sumOfPages; i++) {
+    pagesArray.push(i);
+  }
 
   const handleClick = event => {
     props.getSelectedPage(event.target.id);
     history.push("/data/" + event.target.id);
   };
-
-  
 
   return (
     <div className="pagination-container">
@@ -32,15 +24,16 @@ export default function Pagination(props) {
         //define classes for pagination
         const bold = number === currentPageNumber ? "bold" : null;
         let hidden;
-         if (currentPageNumber.toString().length < 2) {
+        if (currentPageNumber.toString().length < 2) {
           hidden = number < 10 ? "" : "hidden";
-        } 
-        else {
+        } else {
           let tenNumber = Number(currentPageNumber.toString()[0]);
-          hidden = 10 * (tenNumber) <= number && number < 10 * (tenNumber +1) ? "" : "hidden";
-
+          hidden =
+            10 * tenNumber <= number && number < 10 * (tenNumber + 1)
+              ? ""
+              : "hidden";
         }
-       
+
         return (
           <span
             onClick={handleClick}
